54c913c90b83a0f3f59c07eccf2546504ab4922f
Cenz Wong <44856918+cenzwong@users.noreply.github.com>
2022-03-23 22:00:29 +0800

    Update application.py

--- application.py
+++ application.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
         import uvicorn
         import socket
         print("Listening at port 5002 -> IP Address ", socket.gethostbyname(socket.gethostname()))
-        
+        #  socket.gethostbyname_ex(socket.gethostname())[2]
         uvicorn.run("main:app", host="0.0.0.0", workers=1, port=5002)
     except SystemExit as e:
         print('Error!', e)
